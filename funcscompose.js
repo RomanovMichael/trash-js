ab461c79ee6349826ca7b4408b48993093f52b87
Romanov Michael <romanov.players@gmail.com>
2025-05-11 23:16:08 +0300

    add two promises

---
+++ funcscompose.js
@@ -0,0 +1,27 @@
+// )] is fn(x) = f(g(h(x))).
+
+var compose = function(functions) {
+    
+    return function(x) {
+        return functions.reduceRight((acc, curr) => curr(acc), x)
+    }
+};
+
+var compose1 = function(functions) {
+    
+    return function(x) {    
+        let result = x
+
+        for(let i = functions.length - 1;i >=0; i--) {
+            result = functions[i](result)
+        }
+
+        return result
+    }
+};
+
+
+
+
+
+
