429947b48c4916f5a2f902305705d3d7fb1e6812
Romanov Michael <romanov.players@gmail.com>
2025-05-14 22:39:23 +0300

    add interval cancell recursion

--- canselinterval.js
+++ canselinterval.js
@@ -2,14 +2,35 @@ const logFn = (x) => {
     console.log(x * 2);
 }
 
-const cancellable = (fn, args, t) => {
-    fn(...args);
-    const intervalId = setInterval(() => fn(...args), t)
+// const cancellable = (fn, args, t) => {
+//     fn(...args);
+//     const intervalId = setInterval(() => fn(...args), t)
 
-    return () => clearInterval(intervalId) 
+//     return () => clearInterval(intervalId) 
+// }
+
+
+const cancellable1 = (fn, args, t) => {
+    let isCanselled = false;
+    fn(...args);
+
+    const startInterval = () => {
+            setTimeout(() => {
+                if(isCanselled) return
+                fn(...args);
+
+                startInterval()
+        }, t) 
+    }
+
+    startInterval()
+
+    return () => {
+        isCanselled = true;
+    }
 }
 
-const r = cancellable(logFn, [2], 1000);
+const r = cancellable1(logFn, [2], 1000);
 
 const cancelTimeMs = 5000;
 setTimeout(r, cancelTimeMs);
