8a223b24b418bc66307f4f8620a3373466e11996
Romanov Michael <romanov.players@gmail.com>
2025-06-03 09:08:48 +0300

    update 3.06

--- 3-06.js
+++ 3-06.js
@@ -18,6 +18,31 @@ const sumNested = (nums) => {
 }
 
 
+const countNumbers = (nums) => {
+    let count = 0
+    
+    for(let el of nums) {
+        if(typeof el === 'number') {
+            count += 1
+        } else {
+            count += countNumbers(el)
+        }
+    }
+
+    return count
+}
+
+
+
+
+
+
+// console.log(countNumbers([1, [2, [3]], 4])     )    // 👉 4)
+// console.log(countNumbers([[[[5]]]])            )   // 👉 1)
+// console.log(countNumbers([1, [2, [3, [4]]], 5]))   // 👉 5)
+// console.log(countNumbers([])                   )   // 👉 0)
+
+
 // console.log(sumNested([1, [2, [3]], 4])) // 👉 10
 // console.log(sumNested([[[[5]]]]))       // 👉 5
 // console.log(sumNested([1, [2, [3, [4]]], 5])) // 👉 15
