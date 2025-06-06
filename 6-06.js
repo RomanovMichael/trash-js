95996e0b6788716f2533eb3cb290107ee8207a42
Romanov Michael <romanov.players@gmail.com>
2025-06-06 10:10:00 +0300

    update 6.06

--- 6-06.js
+++ 6-06.js
@@ -14,4 +14,19 @@ const countStrings = (arr) => {
     return count
 }
 
-console.log(countStrings(['a', ['b', 3, ['c']], 1, ['d']]))
+const collectNumbers = (arr) => {
+    let resultNums = []
+
+    for(let el of arr) {
+        if(typeof el === 'number') {
+            resultNums.push(el)
+        } else if(Array.isArray(el)) {
+            resultNums.push(...collectNumbers(el))
+        }
+    }
+
+    return resultNums
+}
+
+console.log(collectNumbers([1, 'a', [2, 'b', [3, 4]], 'c']))
+// console.log(countStrings(['a', ['b', 3, ['c']], 1, ['d']]))
