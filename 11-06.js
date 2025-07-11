57d805274861e77b41a5c2d75bf0d42c2be0a3de
Romanov Michael <romanov.players@gmail.com>
2025-07-11 09:39:59 +0300

    add count numbers

--- 11-06.js
+++ 11-06.js
@@ -28,3 +28,18 @@ const flatten = (nums) => {
 
 // Верни массив без вложенности
 console.log(flatten([1, [2, [3, [4]]]]))
+
+
+const countNumbers = (nums) => {
+    return nums.reduce((acc, curr) => {
+        if(Array.isArray(curr)) {
+            acc += countNumbers(curr)
+        } else if (typeof curr === 'number') {
+            acc += 1
+        }
+
+        return acc
+    }, 0)
+}
+// Считай сколько чисел всего во вложенных массивах
+console.log(countNumbers([1, [2, [3, [4, 5]]]])) // → 5
